877386dd5ce6bf3ae43e12a4449ee497a196bcad
Ashish Yadav <18111862+ashiishme@users.noreply.github.com>
2023-08-14 23:12:24 +0200

    :recycle: remove roots props from config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  roots: ['<rootDir>/src'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleDirectories: ['node_modules', 'src']
