425aa31723a25c7d9582ce8b148d1ff8d45e6681
Kishor U <upadhyaya13kishor@gmail.com>
2023-08-15 22:11:01 +0545

    test: added tests for parser

---
+++ src/parser.test.ts
@@ -0,0 +1,143 @@
+import { parse } from './parser'
+describe('Date Parsing', () => {
+  it('should parse YYYY format', () => {
+    const result = parse('2080')
+    expect(result).toEqual({ year: 2080 })
+  })
+
+  it('should parse YYYY-MM format', () => {
+    const result = parse('2080-08')
+    expect(result).toEqual({ year: 2080, month: 8 })
+  })
+
+  it('should parse YYYY/MM format', () => {
+    const result = parse('2080/08')
+    expect(result).toEqual({ year: 2080, month: 8 })
+  })
+
+  it('should parse YYYY MM format', () => {
+    const result = parse('2080 08')
+    expect(result).toEqual({ year: 2080, month: 8 })
+  })
+
+  it('should parse YYYY-MM-DD format', () => {
+    const result = parse('2080-08-15')
+    expect(result).toEqual({ year: 2080, month: 8, day: 15 })
+  })
+
+  it('should parse YYYY/MM/DD format', () => {
+    const result = parse('2080/08/15')
+    expect(result).toEqual({ year: 2080, month: 8, day: 15 })
+  })
+
+  it('should parse YYYY MM DD format', () => {
+    const result = parse('2080 08 15')
+    expect(result).toEqual({ year: 2080, month: 8, day: 15 })
+  })
+
+  it('should parse DD MMMM YYYY format', () => {
+    const result = parse('15 Mangsir 2080')
+    expect(result).toEqual({ year: 2080, month: 8, day: 15 })
+  })
+
+  it('should parse DD-MMMM-YYYY format', () => {
+    const result = parse('15-Mangsir-2080')
+    expect(result).toEqual({ year: 2080, month: 8, day: 15 })
+  })
+
+  it('should parse DD/MMMM/YYYY format', () => {
+    const result = parse('15/Mangsir/2080')
+    expect(result).toEqual({ year: 2080, month: 8, day: 15 })
+  })
+
+  it('should parse DD-MM-YYYY format', () => {
+    const result = parse('15-08-2080')
+    expect(result).toEqual({ year: 2080, month: 8, day: 15 })
+  })
+
+  it('should parse DD/MM/YYYY format', () => {
+    const result = parse('15/08/2080')
+    expect(result).toEqual({ year: 2080, month: 8, day: 15 })
+  })
+
+  it('should parse DD MM YYYY format', () => {
+    const result = parse('15 08 2080')
+    expect(result).toEqual({ year: 2080, month: 8, day: 15 })
+  })
+  it('should parse MM-DD format', () => {
+    const result = parse('08-15')
+    expect(result).toEqual({ month: 8, day: 15 })
+  })
+  it('should parse MM/DD format', () => {
+    const result = parse('08/15')
+    expect(result).toEqual({ month: 8, day: 15 })
+  })
+
+  it('should parse MM DD format', () => {
+    const result = parse('08 15')
+    expect(result).toEqual({ month: 8, day: 15 })
+  })
+
+  it('should parse YY-MM-DD format', () => {
+    const result = parse('80-08-15')
+    expect(result).toEqual({ year: 2080, month: 8, day: 15 })
+  })
+  it('should parse YY/MM/DD format', () => {
+    const result = parse('80/08/15')
+    expect(result).toEqual({ year: 2080, month: 8, day: 15 })
+  })
+
+  it('should parse YY MM DD format', () => {
+    const result = parse('80 08 15')
+    expect(result).toEqual({ year: 2080, month: 8, day: 15 })
+  })
+
+  it('should parse YYY-MM-DD format', () => {
+    const result = parse('080-08-15')
+    expect(result).toEqual({ year: 2080, month: 8, day: 15 })
+  })
+
+  it('should parse YYY/MM/DD format', () => {
+    const result = parse('079/08/15')
+    expect(result).toEqual({ year: 2079, month: 8, day: 15 })
+  })
+
+  it('should parse YYY MM DD format', () => {
+    const result = parse('079 08 15')
+    expect(result).toEqual({ year: 2079, month: 8, day: 15 })
+  })
+  it('should handle mismatched separators (YYYY-MM/DD)', () => {
+    const result = parse('2079-08/15')
+    expect(result).toEqual({ year: 2079, month: 8, day: 15 })
+  })
+
+  it('should handle mismatched separators (YY-MM/DD)', () => {
+    const result = parse('79-08/15')
+    expect(result).toEqual({ year: 2079, month: 8, day: 15 })
+  })
+
+  it('should handle mismatched separators (DD-MM/YYYY)', () => {
+    const result = parse('15-08/2079')
+    expect(result).toEqual({ year: 2079, month: 8, day: 15 })
+  })
+
+  it('should handle invalid date format', () => {
+    const result = parse('invalid date')
+    expect(result).toEqual('invalid date')
+  })
+
+  it('should handle day out of range', () => {
+    const result = parse('2079-02-32')
+    expect(result).toEqual('invalid date')
+  })
+
+  it('should handle month out of range', () => {
+    const result = parse('2079-15-15')
+    expect(result).toEqual('invalid date')
+  })
+
+  it('should handle month out of range with month name', () => {
+    const result = parse('2079-15-15')
+    expect(result).toEqual('invalid date')
+  })
+})
