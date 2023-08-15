0da0bd5f48d36b1cfbc6e8efef7117ee47170587
Kishor U <upadhyaya13kishor@gmail.com>
2023-08-15 22:18:10 +0545

    refactored date Format object

--- src/data/date-formats.ts
+++ src/data/date-formats.ts
@@ -1,73 +1,48 @@
-type Order = 'year' | 'month' | 'day'
+/**
+ * Date separator characters
+ */
+type DateSeparator = '-' | '/' | ' ' | '. '
 
 /**
  * Date formats supported by the parser and formatter
  */
 export type Format =
   | 'YYYY'
-  | 'YYYY-MM'
-  | 'YYYY/MM'
-  | 'YYYY MM'
-  | 'YYYY-MM-DD'
-  | 'YYYY/MM/DD'
-  | 'YYYY MM DD'
-  | 'DD MMMM YYYY'
-  | 'DD-MMMM-YYYY'
-  | 'DD/MMMM/YYYY'
-  | 'DD-MM-YYYY'
-  | 'DD/MM/YYYY'
-  | 'DD MM YYYY'
-  | 'MM-DD'
-  | 'MM/DD'
-  | 'MM DD'
-  | 'YY-MM-DD'
-  | 'YY/MM/DD'
-  | 'YY MM DD'
-  | 'YYY-MM-DD'
-  | 'YYY/MM/DD'
-  | 'YYY MM DD'
+  | `YYYY${DateSeparator}MM`
+  | `${'YYYY' | 'YYY' | 'YY'}${DateSeparator}MM${DateSeparator}DD`
+  | `DD${DateSeparator}${'MMMM' | 'MM'}${DateSeparator}YYYY`
+  | `MM${DateSeparator}DD`
 
 interface DateFormat {
   regex: RegExp
-  format: Format[]
-  order: Order[]
+  format: Format // General format of the date
 }
 
+/**
+ * Date formats supported by the parser and formatter
+ * @internal Used by the parser and formatter
+ * @ignore Exclude from docs
+ */
 export const DateFormats: DateFormat[] = [
-  { regex: /^(\d{4})$/, format: ['YYYY'], order: ['year'] },
+  { regex: /^(\d{4})$/, format: 'YYYY' },
   {
     regex: /^(\d{4})(?:[-/ ](\d{1,2}))$/,
-    format: ['YYYY-MM', 'YYYY/MM', 'YYYY MM'],
-    order: ['year', 'month']
-  },
-  {
-    regex: /^(\d{4})(?:[-/ ](\d{1,2}))(?:[-/ ](\d{1,2}))$/,
-    format: ['YYYY-MM-DD', 'YYYY/MM/DD', 'YYYY MM DD'],
-    order: ['year', 'month', 'day']
-  },
-  {
-    regex: /^(\d{1,2})(?:[-/ ]([A-Za-z]+))(?:[-/ ](\d{4}))$/,
-    format: ['DD MMMM YYYY', 'DD-MMMM-YYYY', 'DD/MMMM/YYYY'],
-    order: ['day', 'month', 'year']
-  },
-  {
-    regex: /^(\d{1,2})(?:[-/ ](\d{1,2}))(?:[-/ ](\d{4}))$/,
-    format: ['DD-MM-YYYY', 'DD/MM/YYYY', 'DD MM YYYY'],
-    order: ['day', 'month', 'year']
-  },
-  {
-    regex: /^(\d{1,2})(?:[-/ ](\d{1,2}))$/,
-    format: ['MM-DD', 'MM/DD', 'MM DD'],
-    order: ['month', 'day']
+    format: 'YYYY-MM'
   },
   {
     regex: /^(\d{2,4})(?:[-/ ](\d{1,2}))(?:[-/ ](\d{1,2}))$/,
-    format: ['YY-MM-DD', 'YY/MM/DD', 'YY MM DD'],
-    order: ['year', 'month', 'day']
+    format: 'YYYY-MM-DD'
   },
   {
-    regex: /^(\d{3,4})(?:[-/ ](\d{1,2}))(?:[-/ ](\d{1,2}))$/,
-    format: ['YYY-MM-DD', 'YYY/MM/DD', 'YYY MM DD'],
-    order: ['year', 'month', 'day']
+    regex: /^(\d{1,2})(?:[-/ ]([A-Za-z]+))(?:[-/ ](\d{4}))$/,
+    format: 'DD-MMMM-YYYY'
+  },
+  {
+    regex: /^(\d{1,2})(?:[-/ ](\d{1,2}))(?:[-/ ](\d{4}))$/,
+    format: 'DD-MM-YYYY'
+  },
+  {
+    regex: /^(\d{1,2})(?:[-/ ](\d{1,2}))$/,
+    format: 'MM-DD'
   }
 ]
