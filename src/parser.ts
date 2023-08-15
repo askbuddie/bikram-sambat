81c94598f2583226e4526bd8ca07f100993f520f
Kishor U <upadhyaya13kishor@gmail.com>
2023-08-15 22:47:04 +0545

    refactor

--- src/parser.ts
+++ src/parser.ts
@@ -1,5 +1,5 @@
-import { NepaliMonthsData, DateFormats } from './data'
-import { generateDateFormatOrder, isDayValid } from 'utils'
+import { NepaliMonthsData, DateFormats, DaysInMonthsMappingData } from './data'
+import { generateDateFormatOrder } from 'utils'
 
 const MonthNames = NepaliMonthsData.map((month) => month.en)
 
@@ -52,4 +52,25 @@ export const parse = (dateString: string): ParseResult | string => {
     }
   }
   return 'invalid date'
+}
+
+/**
+ * Checks if a given day is valid for a given year and month.
+ *
+ * @param {number} year - The year.
+ * @param {number} month - The month (1-12).
+ * @param {number} day - The day.
+ * @returns {boolean} `true` if the day is valid, otherwise `false`.
+ */
+export const isDayValid = (year?: number, month?: number, day?: number) => {
+  if (month && (month < 0 || month > 12)) {
+    return false
+  }
+  if (year && month && day && year < 2100 && year > 1975) {
+    const daysInGivenMonth = DaysInMonthsMappingData[year][month - 1]
+    if (day > daysInGivenMonth) {
+      return false
+    }
+  }
+  return true
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,1 @@
 export * from './generateDateFormatOrder'
-export * from './isDayValid'
