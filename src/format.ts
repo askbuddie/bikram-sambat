a2cd3f9a283b6b2d09b8d6a4e64b3be5795cfbd4
Kishor U <upadhyaya13kishor@gmail.com>
2023-08-17 22:51:33 +0545

    renamed variable name

--- src/format.ts
+++ src/format.ts
@@ -1,21 +1,21 @@
 import BikramSambat from 'BikramSambat'
-import { NepaliMonthsData, type Format } from 'data'
+import { NepaliMonthsData, type DateFormat } from 'data'
 import { generateDateFormatOrder } from 'utils/generateDateFormatOrder'
 
-const MonthNames = NepaliMonthsData.map((month) => month.en)
+const MonthNamesEn = NepaliMonthsData.map((month) => month.en)
 
 /**
  * Formats a BikramSambat date into the specified format.
  *
  * @param {BikramSambat} date - The BikramSambat date to be formatted.
- * @param {Format} dateFormat - The desired format for formatting the date.
+ * @param {DateFormat} dateFormat - The desired format for formatting the date.
  * @returns {string} - The formatted date string.
- * @example <caption>Format a BikramSambat date into `YYYY-MM-DD` format.</caption>
- * const date = new BikramSambat(2077, 1, 1)
+ * @example <caption>DateFormat a BikramSambat date into `YYYY-MM-DD` format.</caption>
+ * const date = new BikramSambat('2077-01-01')
  * const formattedDate = format(date, 'YYYY-MM-DD')
  * console.log(formattedDate) // 2077-01-01
  */
-export const format = (date: BikramSambat, dateFormat: Format): string => {
+export const format = (date: BikramSambat, dateFormat: DateFormat): string => {
   if (date.toString() === 'Invalid Date') {
     return 'Invalid Date'
   }
@@ -30,8 +30,8 @@ export const format = (date: BikramSambat, dateFormat: Format): string => {
       formattedDate = formattedDate.replace('YYY', year.toString().slice(-3))
       formattedDate = formattedDate.replace('YY', year.toString().slice(-2))
     } else if (component === 'month' && month) {
-      if (MonthNames[month - 1]) {
-        formattedDate = formattedDate.replace('MMMM', MonthNames[month - 1])
+      if (MonthNamesEn[month - 1]) {
+        formattedDate = formattedDate.replace('MMMM', MonthNamesEn[month - 1])
         formattedDate = formattedDate.replace(
           'MM',
           month.toString().padStart(2, '0')
