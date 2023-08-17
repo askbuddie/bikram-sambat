5703326e56f6ff802bf69389fd97e25ac04e1a19
Kishor U <upadhyaya13kishor@gmail.com>
2023-08-17 12:31:07 +0545

    refactored Bikram Sambat and added some test cases

--- src/BikramSambat.test.ts
+++ src/BikramSambat.test.ts
@@ -10,4 +10,48 @@ describe('BikramSambat Class', () => {
     const bikramSambat = new BikramSambat('2075-01-01')
     expect(bikramSambat.format('YYYY-MM-DD')).toBe('2075-01-01')
   })
+
+  it('should return correct string .toString()', () => {
+    const bikramSambat = new BikramSambat('2075-01-01')
+    expect(bikramSambat.toString()).toBe('2075-01-01')
+  })
+
+  it('should return correct year .getYear()', () => {
+    const bikramSambat = new BikramSambat('2075-01-01')
+    expect(bikramSambat.getYear()).toBe(2075)
+  })
+
+  it('should return correct month .getMonth()', () => {
+    const bikramSambat = new BikramSambat('2075-01-01')
+    expect(bikramSambat.getMonth()).toBe(1)
+  })
+
+  it('should return correct day .getDay()', () => {
+    const bikramSambat = new BikramSambat('2075-01-01')
+    expect(bikramSambat.getDay()).toBe(1)
+  })
+
+  it('should return Invalid Date .toString()', () => {
+    const bikramSambat = new BikramSambat('207501-01')
+    expect(bikramSambat.toString()).toBe('Invalid Date')
+  })
+
+  it('should return Invalid Date Format .getDay()', () => {
+    const bikramSambat = new BikramSambat('207501-01')
+    expect(bikramSambat.getDay()).toBe(NaN)
+  })
+
+  it('should return Invalid Date Format .getMonth()', () => {
+    const bikramSambat = new BikramSambat('207501-01')
+    expect(bikramSambat.getMonth()).toBe(NaN)
+  })
+  it('should return Invalid Date Format .getYear()', () => {
+    const bikramSambat = new BikramSambat('207501-01')
+    expect(bikramSambat.getYear()).toBe(NaN)
+  })
+
+  it('should return NaN for Invalid Day', () => {
+    const bikramSambat = new BikramSambat('2075-01-32')
+    expect(bikramSambat.getDay()).toBe(NaN)
+  })
 })
--- src/BikramSambat.ts
+++ src/BikramSambat.ts
@@ -15,9 +15,9 @@ export default class BikramSambat {
   // private readonly newYearMap = NewYearMappingData
   // private readonly daysInMonthMap = DaysInMonthsMappingData
 
-  private year: number
-  private month: number
-  private day: number
+  private year: number | undefined
+  private month: number | undefined
+  private day: number | undefined
 
   constructor(dateStr?: string | BikramSambat) {
     /* TODO: this needs to be changed to default to current date in 
@@ -28,36 +28,36 @@ export default class BikramSambat {
     if (typeof dateStr === 'string') {
       const parsedDate = parse(dateStr)
       if (typeof parsedDate === 'string') {
-        this.year = 0
-        this.month = 0
-        this.day = 0
+        this.year = undefined
+        this.month = undefined
+        this.day = undefined
       } else {
         const { year, month, day } = parsedDate
-        this.year = year || 0
-        this.month = month || 0
-        this.day = day || 0
+        this.year = year ?? undefined
+        this.month = month ?? undefined
+        this.day = day ?? undefined
       }
     } else if (dateStr instanceof BikramSambat) {
       this.year = dateStr.getYear()
       this.month = dateStr.getMonth()
       this.day = dateStr.getDay()
     } else {
-      this.year = 0
-      this.month = 0
-      this.day = 0
+      this.year = undefined
+      this.month = undefined
+      this.day = undefined
     }
   }
 
   public getYear(): number {
-    return this.year
+    return this.year ?? NaN
   }
 
   public getMonth(): number {
-    return this.month
+    return this.month ?? NaN
   }
 
   public getDay(): number {
-    return this.day
+    return this.day ?? NaN
   }
 
   public format(formatStr: Format): string {
@@ -65,9 +65,19 @@ export default class BikramSambat {
   }
 
   toString(): string {
-    if (!isDayValid(this.year, this.month, this.day)) {
-      return 'invalid date'
+    if (
+      this.year === undefined ||
+      this.month === undefined ||
+      this.day === undefined
+    ) {
+      return 'Invalid Date'
     }
-    return `${this.year}-${this.month}-${this.day}`
+    if (!isDayValid(this.year, this.month, this.day)) {
+      return 'Invalid Date'
+    }
+    // the month and day are padded with 0 if they are less than 10
+    const month = this.month <= 9 ? '0' + this.month : this.month
+    const day = this.day <= 9 ? '0' + this.day : this.day
+    return `${this.year}-${month}-${day}`
   }
 }
