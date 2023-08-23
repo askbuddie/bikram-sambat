774a0cd1d66e7397153612f04e749a71a55c57dc
Kishor U <upadhyaya13kishor@gmail.com>
2023-08-23 10:01:03 +0545

    file renames

--- src/utils/getDaysBetweenTwoAdDates.test.ts
+++ src/utils/getDaysBetweenTwoAdDates.test.ts
@@ -1,21 +1,21 @@
-import { getDaysBetweenTwoDatesAD } from './getDaysBetweenTwoDatesAD'
+import { getDaysBetweenTwoAdDates } from './getDaysBetweenTwoAdDates'
 
-describe('getDaysBetweenTwoDatesAD', () => {
+describe('getDaysBetweenTwoAdDates', () => {
   it('calculates the correct number of days for positive difference', () => {
     const startDate = new Date('2023-08-15')
     const endDate = new Date('2023-09-01')
-    expect(getDaysBetweenTwoDatesAD(startDate, endDate)).toBe(17)
+    expect(getDaysBetweenTwoAdDates(startDate, endDate)).toBe(17)
   })
 
   it('calculates the correct number of days for negative difference', () => {
     const startDate = new Date('2023-09-01')
     const endDate = new Date('2023-08-15')
-    expect(getDaysBetweenTwoDatesAD(startDate, endDate)).toBe(17)
+    expect(getDaysBetweenTwoAdDates(startDate, endDate)).toBe(17)
   })
 
   it('calculates the correct number of days for same dates', () => {
     const startDate = new Date('2023-08-15')
     const endDate = new Date('2023-08-15')
-    expect(getDaysBetweenTwoDatesAD(startDate, endDate)).toBe(0)
+    expect(getDaysBetweenTwoAdDates(startDate, endDate)).toBe(0)
   })
 })
