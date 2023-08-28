377e6c76dbad0e83c58740d5ed4356c14ec5a9b2
thebishalniroula <niroulabishal199@gmail.com>
2023-08-28 22:30:42 +0545

    code refactor + logo added to nav bar

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-
+import Image from 'next/image'
 const config: DocsThemeConfig = {
-  logo: <span>Bikram sambat</span>,
+  logo: (
+    <div style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+      <Image src={'/logo.png'} height={50} width={50} alt="askbuddie logo" />
+      Bikram Sambat
+    </div>
+  ),
   project: {
     link: 'https://github.com/askbuddie/bikram-sambat'
   },
   chat: {
-    link: 'https://discord.com/channels/735870405709135924'
+    link: 'https://dsc.gg/askbuddie'
   },
-  docsRepositoryBase: 'https://github.com/askbuddie/bikram-sambat-docs',
+  docsRepositoryBase: 'https://github.com/askbuddie/bikram-sambat',
   footer: {
-    text: 'Bikram Sambat by Askbuddie'
+    text: 'Bikram Sambat by Ask Buddie'
   },
   primaryHue: {
     light: 15,
