284b352e50522756d45f078b371e3cab531c87e9
thebishalniroula <niroulabishal199@gmail.com>
2023-08-28 21:51:24 +0545

    docs: initilized

---
+++ docs/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Bikram sambat</span>,
+  project: {
+    link: "https://github.com/askbuddie/bikram-sambat",
+  },
+  chat: {
+    link: "https://discord.com/channels/735870405709135924",
+  },
+  docsRepositoryBase: "https://github.com/askbuddie/bikram-sambat-docs",
+  footer: {
+    text: "Bikram Sambat by Askbuddie",
+  },
+};
+
+export default config;
