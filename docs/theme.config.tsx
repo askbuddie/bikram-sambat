656bda7e1a32c02a1efc5e8e5a8580d8d43d5230
thebishalniroula <niroulabishal199@gmail.com>
2023-08-28 22:15:26 +0545

    code refactor + white theme according askbuddie logo color

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,18 +1,22 @@
-import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Bikram sambat</span>,
   project: {
-    link: "https://github.com/askbuddie/bikram-sambat",
+    link: 'https://github.com/askbuddie/bikram-sambat'
   },
   chat: {
-    link: "https://discord.com/channels/735870405709135924",
+    link: 'https://discord.com/channels/735870405709135924'
   },
-  docsRepositoryBase: "https://github.com/askbuddie/bikram-sambat-docs",
+  docsRepositoryBase: 'https://github.com/askbuddie/bikram-sambat-docs',
   footer: {
-    text: "Bikram Sambat by Askbuddie",
+    text: 'Bikram Sambat by Askbuddie'
   },
-};
+  primaryHue: {
+    light: 15,
+    dark: null
+  }
+}
 
-export default config;
+export default config
